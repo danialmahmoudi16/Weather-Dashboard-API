4197e1dd0730e60000eb885b8de07a6c4b6fee68
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-07-02 12:09:25 -0400

    Function to display current weather data is working now. Functions have been created for unit conversions as well.

--- assets/script.js
+++ assets/script.js
@@ -11,63 +11,84 @@ const currentHumidity = document.getElementById('current-humidity');
 const fiveDayForecast = document.getElementById('five-day-forecast');
 const forecastColumns = document.getElementById('5-day-forecast-columns');
 
-// fetch current weather data
-function fetchCurrentWeather(city) {
+
+function fetchWeatherData(city) {
     const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=a2986e392fb8f98a746af14b93d91f5a';
     fetch(apiUrl)
     .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        currentWeather.textContent = data.city.name;
-        currentTemp.textContent = 'Temp:' + data.current.temp + '°F';
-        currentWind.textContent = 'Wind:' + data.current.wind_speed + 'MPH';
-        currentHumidity.textContent = 'Humidity:' + data.current.humidity + '%';
-    });
-}
-
-// fetch 5 day forecast data
-function fetchFiveDayForecast(city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-    fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        fiveDayForecast.innerHTML = '<h2>5-Day Forecast:</h2>';
-        forecastColumns.innerHTML = '';
-        for (let i = 0; i < data.forecast.forecastday.length; i++) {
-            const forecast = data.forecastday[i];
-            const column = document.createElement('div');
-            column.classList.add('column');
-            column.innerHTML = `
-            <div class="col--12 mb-3">
-            <div class="date">${forecast.date}</div>
-            <div class="icon">
-             <img src="${forecast.day.condition.icon}" 
-             alt="weather Icon">
-             </div>
-             <div class="temp">Temp: ${forecast.day.avgtemp_f}°F</div>
-             <div class="wind">Wind: ${forecast.day.maxwind_mph}MPH</div>
-             <div class="humidity">Humidity: ${forecast.day.avghumidity}%</div>
-                </div>
-             `;
-                forecastColumns.appendChild(column);
+        if (!response.ok) {
+            throw response.json('No weather data found');
         }
+        return response.json();
+    })
+    .then(function (data) {
+        displayCurrentWeather(data);
+    })
+    .catch(function (error) {
+        alert('Unable to connect to OpenWeather');
     });
 }
+
+
+
+// fetch current weather data
+function displayCurrentWeather(weatherData) {
+
+        currentWeather.textContent = weatherData.name;
+        currentTemp.textContent = 'Temp:' + convertKelvinToFarenheit(weatherData.main.temp) + ' °F ';
+        currentWind.textContent = 'Wind:' + convertMetersPerSecondToMPH(weatherData.wind.speed) + ' MPH ';
+        currentHumidity.textContent = 'Humidity:' + weatherData.main.humidity + ' % ';
+    };
+
+
+// // fetch 5 day forecast data
+// function fetchFiveDayForecast(city) {
+//     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+//     fetch(apiUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         fiveDayForecast.innerHTML = '<h2>5-Day Forecast:</h2>';
+//         forecastColumns.innerHTML = '';
+//         for (let i = 0; i < data.forecast.forecastday.length; i++) {
+//             const forecast = data.forecastday[i];
+//             const column = document.createElement('div');
+//             column.classList.add('column');
+//             column.innerHTML = `
+//             <div class="col--12 mb-3">
+//             <div class="date">${forecast.date}</div>
+//             <div class="icon">
+//              <img src="${forecast.day.condition.icon}" 
+//              alt="weather Icon">
+//              </div>
+//              <div class="temp">Temp: ${forecast.day.avgtemp_f}°F</div>
+//              <div class="wind">Wind: ${forecast.day.maxwind_mph}MPH</div>
+//              <div class="humidity">Humidity: ${forecast.day.avghumidity}%</div>
+//                 </div>
+//              `;
+//                 forecastColumns.appendChild(column);
+//         }
+//     });
+// }
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
-    const city = cityInput.value.trim();
-    if (city) {
-        fetchCurrentWeather(city);
-        fetchFiveDayForecast(city);
-        cityInput.value = '';
-    } else {
-        alert('Please enter a city');
+    const userSearch = cityInput.value.trim();
+    cityInput.value = '';
+
+    if (userSearch) {
+        fetchWeatherData(userSearch);
     }
+}
+
+function convertKelvinToFarenheit(kelvin) {
+    return Math.round((kelvin - 273.15) * 9/5 + 32);
+}
+
+function convertMetersPerSecondToMPH(metersPerSecond) {
+    return Math.round(metersPerSecond * 2.237);
 }
 
 searchBtn.addEventListener('click', handleSearchFormSubmit);
